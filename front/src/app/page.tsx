75bbcc00ebe881b803e0ff9f633d7dabc53bb690
root <root@DESKTOP-LV8K6Q8.localdomain>
2023-10-19 21:29:25 +0900

    init

---
+++ front/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </main>
+  )
+}
